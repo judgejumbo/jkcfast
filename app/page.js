1a5511e991eec8a9a1631eb43fb1c82a2f3c8daf
judgejumbo <jimbokennedy2018@gmail.com>
2024-12-08 08:16:15 +1000

    JJP Change button in 2025

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Get started</button>
+      <button className="btn">Get started Now in 2025</button>
     </main>
   );
 }
