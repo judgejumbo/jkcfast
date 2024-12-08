6ea8b1d25ec2ee68d0a11f7fd7286288c7df8757
judgejumbo <jimbokennedy2018@gmail.com>
2024-12-08 19:33:57 +1000

    Start Tailwind Style

--- app/page.js
+++ app/page.js
@@ -1,15 +1,19 @@
-import Link from "next/link";
+import Bnlogin from "@/components/ButtonLogin_1";
 
 export default function Home() {
   return (
     <main>
-      <h1>Collect Customer Feedback To Build Better Products</h1>
+      <h1 style={{ fontSize: "48px" }}>
+        {" "}
+        Collect Customer Feedback To Build Better Products
+      </h1>
 
       <div>
         Create a feedback board in minutes. This lets you prioritze developent
         to build products CUSTOMERS will love
       </div>
-      <Link href="/dashboard"> Dashboard</Link>
+
+      <Bnlogin></Bnlogin>
     </main>
   );
 }
---
+++ components/ButtonLogin_1.js
@@ -0,0 +1,6 @@
+import Link from "next/link";
+
+const Bnlogin = () => {
+  return <Link href="/dashboard"> Dashboard</Link>;
+};
+export default Bnlogin;
