d44cb1e449524284d418b87a9c07a2284c4e5bb9
judgejumbo <jimbokennedy2018@gmail.com>
2024-12-10 07:32:38 +1000

    Card - Price Section

--- app/page.js
+++ app/page.js
@@ -6,11 +6,12 @@ export default function Home() {
 
   return (
     <main>
+      {/* HEADER */}
       <section className="bg-gray-200">
-        <div className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2">
+        <div className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2 mb-4">
           {" "}
           <div className="font-bold"> JJP CodeFast</div>
-          <div className="space-x-4">
+          <div className="space-x-4 hidden md:block">
             <a>Pricing</a>
             <a>FAQ</a>
           </div>
@@ -19,8 +20,9 @@ export default function Home() {
           </div>
         </div>
       </section>
+      {/* HERO */}
       <section className="p-8 text-center py-32 max-w-3xl mx-auto">
-        <h1 className="text-4xl font-extrabold mb-6">
+        <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
           {" "}
           Collect Customer Feedback To Build Better Products
         </h1>
@@ -30,6 +32,34 @@ export default function Home() {
           to build products CUSTOMERS will love
         </div>
         <Bnlogin isLoggedIn={isLoggedIn} name={name}></Bnlogin>
+      </section>
+      {/* Pricing */}
+      <section className="bg-gray-200">
+        <div className="p-8 py-32 max-w-3xl mx-auto">
+          <p className="text-sm uppercase font-medium text-center text-primary">
+            Pricing
+          </p>
+          <h2 className="text-3xl lg:text-3xl font-extrabold mb-12 text-center">
+            A pricing that adapts to your needs
+          </h2>
+          {/* card */}
+          <div className="p-8 bg-base-100 w-96 rounded-3xl mx-auto">
+            <div className="flex gap-2 items-baseline">
+              <div className="text-4xl font-black"> $19</div>
+              <div className="uppercase text-sm font-medium opacity-50">
+                {" "}
+                /month
+              </div>
+            </div>
+
+            <ul>
+              <li>Collect Customer Feedback</li>
+              <li>Collect Customer Feedback</li>
+              <li>Collect Customer Feedback</li>
+              <li>Collect Customer Feedback</li>
+            </ul>
+          </div>
+        </div>
       </section>
     </main>
   );
