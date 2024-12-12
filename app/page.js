26d5e891bcdde67336a054c43b4fb27ed5627aa3
judgejumbo <jimbokennedy2018@gmail.com>
2024-12-12 21:20:23 +1000

    Pages Responsive - Finish

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import Bnlogin from "@/components/ButtonLogin_1";
 import FAQListItem from "@/components/FAQListItem";
+import Image from "next/image";
+import productDemo from "./productDemo.jpeg";
 
 export default function Home() {
   const isLoggedIn = true;
@@ -9,14 +11,14 @@ export default function Home() {
     "Collect Customer Feedback",
     "Unlimited Boards",
     "Admin Dashborad",
-    "24/7/Support",
+    "24/7/ Support",
   ];
 
   return (
     <main>
       {/* HEADER */}
       <section className="bg-gray-200">
-        <div className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2 mb-4">
+        <div className="max-w-5xl mx-auto flex justify-between items-center px-8 py-2 mb-4">
           {" "}
           <div className="font-bold"> JJP CodeFast</div>
           <div className="space-x-4 hidden md:block">
@@ -33,17 +35,25 @@ export default function Home() {
         </div>
       </section>
       {/* HERO */}
-      <section className="p-8 text-center py-32 max-w-3xl mx-auto">
-        <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
-          {" "}
-          Collect Customer Feedback To Build Better Products
-        </h1>
+      <section className="text-center lg:text-left py-32 px-8 max-w-5xl mx-auto flex flex-col lg:flex-row gap-14 items-center lg:items-start">
+        <Image
+          src={productDemo}
+          alt="Product Demo Judge"
+          className="w-96 rounded-xl"
+        />
 
-        <div className="opacity-90 mb-10">
-          Create a feedback board in minutes. This lets you prioritze developent
-          to build products CUSTOMERS will love
+        <div>
+          {" "}
+          <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
+            {" "}
+            Collect Customer Feedback To Build Better Products
+          </h1>
+          <div className="opacity-90 mb-10">
+            Create a feedback board in minutes. This lets you prioritze
+            developent to build products CUSTOMERS will love
+          </div>
+          <Bnlogin isLoggedIn={isLoggedIn} name={name}></Bnlogin>
         </div>
-        <Bnlogin isLoggedIn={isLoggedIn} name={name}></Bnlogin>
       </section>
       {/* Pricing */}
       <section className="bg-gray-200" id="pricing">
