9690b220bf195ca7e109cb0d69865fd72524b181
judgejumbo <jimbokennedy2018@gmail.com>
2024-12-18 07:29:47 +1000

    SignOut Lesson
    Seems to be working

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,13 +1,18 @@
-import Link from "next/link";
+import ButtonLogout from "@/components/ButtinLogout";
+
+// import Link from "next/link"; - It used to be in there
+
 export default function Dashboard() {
   return (
     <main>
       <h1 className="text-2xl font-bold border-solid bg-lime-400 border-4 border-slate-600 p-2 text-center">
         {" "}
-        Dashboard{" "}
+        Private Dashboard{" "}
       </h1>
 
-      <Link href="/"> Home</Link>
+      <ButtonLogout />
+
+      {/* <Link href="/"> Home</Link> */}
     </main>
   );
 }
