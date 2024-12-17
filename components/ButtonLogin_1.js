8daecd905cfc189f5de759f771e89b4b38240430
judgejumbo <jimbokennedy2018@gmail.com>
2024-12-17 17:40:32 +1000

    End of Sign In Lesson
    Login worrked

--- components/ButtonLogin_1.js
+++ components/ButtonLogin_1.js
@@ -1,21 +1,31 @@
+"use client";
 import Link from "next/link";
-
-// this button sends people to the login or dashboard
-
-// destructing the isLoggedIn
+import { signIn } from "next-auth/react";
 
 const Bnlogin = ({ session, extraStyle }) => {
+  const dashBoardUrl = "/dashboard";
+
   if (session) {
     return (
       <Link
-        href="/dashboard "
+        href={dashBoardUrl}
         className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
       >
         {" "}
-        Welcome Back {session.user.name}
+        Welcome Back {session.user.name || "friend"}
       </Link>
     );
   }
-  return <button>Login </button>;
+  return (
+    <button
+      className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
+      onClick={() => {
+        signIn(undefined, { callbackUrl: dashBoardUrl });
+      }}
+    >
+      Get Started
+    </button>
+  );
 };
+
 export default Bnlogin;
